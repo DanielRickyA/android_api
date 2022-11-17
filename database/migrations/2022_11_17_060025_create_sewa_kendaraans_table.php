dcd54fbe446132ec3b9dc1f5ab535346bbae39ce
DanielRickyA <danielrickya1404@gmail.com>
2022-11-17 22:49:09 +0700

    first commit

---
+++ database/migrations/2022_11_17_060025_create_sewa_kendaraans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sewa_kendaraans', function (Blueprint $table) {
+            $table->id();
+            $table->string('lokasi');
+            $table->string('tanggalPinjam');
+            $table->string('tanggalKembali');
+            $table->string('modelKendaraan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sewa_kendaraans');
+    }
+};
